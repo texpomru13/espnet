ca4d686357fbb0823617177df3f2216f5060c795
vzykov <vzykov@trademux.net>
2020-02-15 21:21:01 +0500

    symb update

--- egs/ljspeech/tts1/local/text/symbols.py
+++ egs/ljspeech/tts1/local/text/symbols.py
@@ -6,12 +6,12 @@ Defines the set of symbols used in text input to the model.
 The default is a set of ASCII characters that works well for English or text that has been run through Unidecode. For other data, you can modify _characters. See TRAINING_DATA.md for details. '''
 from text import cmudict
 
-_punctuation = '!\'",.:;?()…+ '
+_punctuation = '!\',.:;?()…+ '
 #_math = '#%&*+-/[]()'
 _special = '_~'
 #_accented = 'áçéêëñöøćž'
 #_numbers = '0123456789'
-_letters = 'ЙЦУКЕНГШЩЗХЪЁФЫВАПРОЛДЖЭЯЧСМИТЬБЮйцукенгшщзхъёфывапролджэячсмитьбю' #QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm'
+_letters = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm' #QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm'
 
 # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as
 # uppercase letters):
